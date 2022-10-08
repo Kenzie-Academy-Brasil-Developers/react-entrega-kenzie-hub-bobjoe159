cff1b7e26bb326008289c2b50182d033538e6d43
Pedro Henrique Feitosa <50965091+bobjoe159@users.noreply.github.com>
2022-10-08 17:57:31 -0300

    feat: removido timeout 5s requisição

--- src/services/axios.js
+++ src/services/axios.js
@@ -4,7 +4,6 @@ const token = localStorage.getItem("@kenzieHub:token");
 
 const api = axios.create({
   baseURL: "https://kenziehub.herokuapp.com",
-  timeout: 5000,
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${token}`,
